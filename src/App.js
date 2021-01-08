1aabe20a84d88a31f3d1c0d7c3fdc30cc85b4de8
AyselHuseynovaa <ayselhuseynov3@gmail.com>
2021-01-08 20:26:46 +0400

    random

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import React, { Component } from "react";
+import RandomNum from "./RandomNum";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <RandomNum />
+      </div>
+    )
+  }
+}
+
+export default App;
